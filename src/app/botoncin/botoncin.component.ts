38b3283dc8aadfa3348181c674217505ab992b62
Daniel Toro <ganguis2005@aol.com>
2020-03-16 15:05:12 +0100

    light switch

--- src/app/botoncin/botoncin.component.ts
+++ src/app/botoncin/botoncin.component.ts
@@ -7,12 +7,19 @@ import { Component } from '@angular/core';
 })
 export class BotoncinComponent {
 
-  public buttonColor: string =  "red";
-
+  public buttonColor: string =  "black";
+  public buttonOn: boolean = false;
 
   public printAction($event) {
-    this.buttonColor = "black";
-    console.log('Click', $event)
+    if(!this.buttonOn) {
+      this.buttonOn = true;
+      this.buttonColor = "yellow";
+      console.log('On', $event);
+    } else {
+      this.buttonOn = false;
+      this.buttonColor = "black";
+      console.log('Off', $event);
+    }
   }
 
 }
